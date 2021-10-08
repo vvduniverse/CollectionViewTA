7787b705b33d55e6ecdeff7ba557bd86831892cd
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-10-08 11:09:52 +0300

    Initial Commit

---
+++ CollectionViewTA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewTA
+//
+//  Created by Vahtee Boo on 08.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
